ff7f0951b039e2a17ad51182fb3cf9834727e2e3
dd84ai <dd84ai@gmail.com>
2019-10-05 21:24:35 +0700

    Texted version of output

--- PPNN/Training.py
+++ PPNN/Training.py
@@ -8,37 +8,68 @@ Created on Fri Oct  4 22:17:28 2019
 import cv2 as cv2
 import numpy as np
 
-#######   training part    ############### 
-samples = np.loadtxt('generalsamples.data',np.float32)
-responses = np.loadtxt('generalresponses.data',np.float32)
-responses = responses.reshape((responses.size,1))
-
-model = cv2.ml.KNearest_create()
-model.train(samples,cv2.ml.ROW_SAMPLE,responses)
+def Train():
+    #######   training part    ############### 
+    samples = np.loadtxt('generalsamples.data',np.float32)
+    responses = np.loadtxt('generalresponses.data',np.float32)
+    responses = responses.reshape((responses.size,1))
+    
+    model = cv2.ml.KNearest_create()
+    model.train(samples,cv2.ml.ROW_SAMPLE,responses)
+    return model
+model = Train()
 
 ############################# testing part  #########################
+def sortSecond(val): 
+    return val[1]  
+def Extract():
+    ############################# testing part  #########################
+    
+    im = cv2.imread('pi2.png')
+    gray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+    thresh = cv2.adaptiveThreshold(gray,255,1,1,11,2)
+    
+    contours,hierarchy = cv2.findContours(thresh,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+    arr = [2,]
+    for cnt in contours:
+        if cv2.contourArea(cnt)>20:
+            [x,y,w,h] = cv2.boundingRect(cnt)
+            if  h>8:
+                cv2.rectangle(im,(x,y),(x+w,y+h),(0,255,0),2)
+                roi = thresh[y:y+h,x:x+w]
+                roismall = cv2.resize(roi,(10,10))
+                roismall = roismall.reshape((1,100))
+                roismall = np.float32(roismall)
+                retval, results, neigh_resp, dists = model.findNearest(roismall, k = 1)
+                string = str(int((results[0][0])))
+                arr.append([string,x])
 
-im = cv2.imread('pi2.png')
-out = np.zeros(im.shape,np.uint8)
-gray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
-thresh = cv2.adaptiveThreshold(gray,255,1,1,11,2)
+    #arr = (list(reversed(arr)))
+    leng = len(arr)
+    i = 0
+    while(i < leng):
+        if (type(arr[i]) == type(1)):
+            arr.pop(i)
+            leng -= 1
+        i += 1
+                
+    arr.sort(key = sortSecond) 
+    print(arr)
+    st = ""
+    repeated = []
+    notfound = True
+    if (len(arr)>1):
+        for value in arr:
+            if (type(value) == type(1)):
+                continue
+            notfound = True
+            for j in range(len(repeated)):
+                if (abs(int(value[1])-repeated[j]) < 10):
+                    notfound = False
+                    break
+            if (notfound):
+                st += value[0]
+                repeated.append(int(value[1]))
 
-contours,hierarchy = cv2.findContours(thresh,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
-
-for cnt in contours:
-    if cv2.contourArea(cnt)>50:
-        [x,y,w,h] = cv2.boundingRect(cnt)
-        if  h>28:
-            cv2.rectangle(im,(x,y),(x+w,y+h),(0,255,0),2)
-            roi = thresh[y:y+h,x:x+w]
-            roismall = cv2.resize(roi,(10,10))
-            roismall = roismall.reshape((1,100))
-            roismall = np.float32(roismall)
-            retval, results, neigh_resp, dists = model.findNearest(roismall, k = 1)
-            string = str(int((results[0][0])))
-            print(string, x)
-            cv2.putText(out,string,(x,y+h),0,1,(0,255,0))
-
-cv2.imshow('im',im)
-cv2.imshow('out',out)
-cv2.waitKey(0)
+    return st
+print(Extract())
