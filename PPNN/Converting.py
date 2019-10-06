75884960960c11829517e9cfed454c410cef35db
dd84ai <dd84ai@gmail.com>
2019-10-06 12:58:08 +0700

    Converting

--- PPNN/Converting.py
+++ PPNN/Converting.py
@@ -16,6 +16,17 @@ samples = np.array(samples,np.int32)
 responses = np.array(responses,np.int32)
 
 train = pd.DataFrame(data=samples[1:,1:],index=samples[1:,0],columns=samples[0,1:])
-Y_train = pd.DataFrame(data=responses[1:,1:],index=responses[1:,0],columns=responses[0,1:])
+#Y_train = pd.DataFrame(data=responses[1:,1:],index=responses[1:,0],columns=responses[0,1:])
+
+print(samples)
 print(train)
-print(Y_train)
+#print(Y_train)
+import pandas as pd
+import numpy as np
+data =samples
+data.shape
+type(data)
+df=pd.DataFrame(data=data[0:,0:],
+        index=[i for i in range(data.shape[0])],
+        columns=['f'+str(i) for i in range(data.shape[1])])
+print(df.head())
